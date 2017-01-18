29ac87256d5cf4a99ac6f8e90438b4d1fab47a67
kelrezo <travis_layne@yahoo.com>
2017-01-17 21:35:01 -0600

    controller
    workign on a controller

---
+++ BasicApi/BasicApi/Models/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicApi.Models
+{
+    [Serializable]
+    public class Entity
+    {
+        private readonly string name;
+        private readonly int age;
+        private readonly bool dead;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+        public bool Dead
+        {
+            get
+            {
+                return dead;
+            }
+        }
+    }
+}
